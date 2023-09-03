f8a1d0aca2eef58af75a7646ae55109795cc4602
DanielCoffy <daniel.danielcoffy@gmail.com>
2023-09-03 20:03:15 -0300

    Add files via upload

---
+++ aula12/ex011.js
@@ -0,0 +1,17 @@
+var idade=15
+if (idade<16)
+{
+    console.log('Não vota.')
+}
+else if (idade <18)
+{
+    console.log('Voto opcional.')    
+}
+else if (idade>=18 && idade<65)
+{
+    console.log('Voto obrigatório.')
+}
+else
+{
+    console.log('Voto opcional.')
+}
